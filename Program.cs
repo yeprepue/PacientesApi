3eaaf0d61ce3a387172082ce6a043d15dd1ec002
yeprepue <yepretel@misena.edu.co>
2026-02-25 18:37:51 -0500

    Refactorizar Program.cs para mejorar la claridad y la organización del registro del servicio y la configuración de Swagger.

--- Program.cs
+++ Program.cs
@@ -5,28 +5,28 @@ using PacientesApi.Modules.Pacientes.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Configurar Entity Framework Core con SQL Server
+// Configuración de Base de Datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Registrar el servicio de pacientes
+// Registro del Servicio
 builder.Services.AddScoped<IPacienteService, PacienteService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pacientes API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
